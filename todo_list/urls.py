9e78991b7edca95a13d56b656347dd11cb18ce7c
charmniovi <charmniovi@tutanota.com>
2024-05-22 12:18:05 +0300

    registred TaskSwitchToCompletedView and TaskSwitchToUncompletedView in urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,7 +2,9 @@ from todo_list.views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
-    TaskDeleteView
+    TaskDeleteView,
+    TaskSwitchToCompletedView,
+    TaskSwitchToUncompletedView
 )
 from django.urls import path
 
@@ -12,6 +14,8 @@ urlpatterns = [
     path('create/', TaskCreateView.as_view(), name="task-create"),
     path('task/<int:pk>/update', TaskUpdateView.as_view(), name="task-update"),
     path('task/<int:pk>/delete', TaskDeleteView.as_view(), name="task-delete"),
+    path('task/<int:pk>/switch_to_done', TaskSwitchToCompletedView.as_view(), name="task-switch-to-done"),
+    path('task/<int:pk>/switch_to_undone', TaskSwitchToUncompletedView.as_view(), name="task-switch-to-undone"),
 ]
 
 app_name = "todo_list"
