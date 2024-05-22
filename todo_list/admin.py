3c38ca6dae44447de8ef8818aab1c262e05fcf9d
charmniovi <charmniovi@tutanota.com>
2024-05-22 10:58:39 +0300

    added Tag and Task in admin panel

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from todo_list.models import Task, Tag
 
-# Register your models here.
+admin.site.register(Tag)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Task', {
+            'fields': ['title', 'content', 'deadline', 'done', 'tags']
+        }),
+    ]
+    list_display = ('title', 'created_at', 'deadline', 'done')
+    list_filter = ("done",)
+    search_fields = ('title', "tags__name")
