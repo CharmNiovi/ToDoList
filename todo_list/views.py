b9005dd0758cb6b153593f669643b7a27761d66b
charmniovi <charmniovi@tutanota.com>
2024-05-22 11:47:45 +0300

    added TaskCreateView

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
@@ -8,3 +9,11 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     queryset = Task.objects.prefetch_related("tags").order_by(
         "done", "-created_at"
     )
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = ("title", "content", "deadline", "tags")
+
+    def get_success_url(self):
+        return reverse("todo_list:task_list")
