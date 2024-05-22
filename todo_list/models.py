932c16c810f61d9d6eda8ea2245b1ee8df8dcc98
charmniovi <charmniovi@tutanota.com>
2024-05-22 10:51:25 +0300

    added Task model

--- todo_list/models.py
+++ todo_list/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.content
