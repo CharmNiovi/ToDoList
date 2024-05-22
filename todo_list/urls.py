1ab22f168fd55f30fcbaab77d181cb0875a66f6d
charmniovi <charmniovi@tutanota.com>
2024-05-22 11:48:06 +0300

    registred TaskCreateView in url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,10 @@
-from todo_list.views import TaskListView
+from todo_list.views import TaskListView, TaskCreateView
 from django.urls import path
 
 
 urlpatterns = [
     path('', TaskListView.as_view(), name="task_list"),
+    path('create_new_task', TaskCreateView.as_view(), name="task_create"),
 ]
 
 app_name = "todo_list"
