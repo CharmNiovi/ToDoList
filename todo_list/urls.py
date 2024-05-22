2c95aa587443a2a0384188f671d3cc811bcac5af
charmniovi <charmniovi@tutanota.com>
2024-05-22 12:34:39 +0300

    registred all Tag view

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,7 +4,11 @@ from todo_list.views import (
     TaskUpdateView,
     TaskDeleteView,
     TaskSwitchToCompletedView,
-    TaskSwitchToUncompletedView
+    TaskSwitchToUncompletedView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
 )
 from django.urls import path
 
@@ -12,10 +16,23 @@ from django.urls import path
 urlpatterns = [
     path('', TaskListView.as_view(), name="task-list"),
     path('create/', TaskCreateView.as_view(), name="task-create"),
-    path('task/<int:pk>/update', TaskUpdateView.as_view(), name="task-update"),
-    path('task/<int:pk>/delete', TaskDeleteView.as_view(), name="task-delete"),
-    path('task/<int:pk>/switch_to_done', TaskSwitchToCompletedView.as_view(), name="task-switch-to-done"),
-    path('task/<int:pk>/switch_to_undone', TaskSwitchToUncompletedView.as_view(), name="task-switch-to-undone"),
+    path('task/<int:pk>/update/',
+         TaskUpdateView.as_view(),
+         name="task-update"),
+    path('task/<int:pk>/delete/',
+         TaskDeleteView.as_view(),
+         name="task-delete"),
+    path('task/<int:pk>/switch_to_done/',
+         TaskSwitchToCompletedView.as_view(),
+         name="task-switch-to-done"),
+    path('task/<int:pk>/switch_to_undone/',
+         TaskSwitchToUncompletedView.as_view(),
+         name="task-switch-to-undone"),
+
+    path('tag/', TagListView.as_view(), name="tag-list"),
+    path('tag/create/', TagCreateView.as_view(), name="tag-create"),
+    path('tag/<int:pk>/update/', TagUpdateView.as_view(), name="tag-update"),
+    path('tag/<int:pk>/delete/', TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo_list"
